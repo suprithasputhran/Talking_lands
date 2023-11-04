f6be7af4cece1c8e782a7682bbe59f8d9b878282
suprithasputhran <suprithask5@gmail.com>
2023-11-04 18:26:15 +0530

    first commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 function NavBar() {
     return (
-        <div className='h-screen font-Roboto w-56 pl-9 pt-24  bg-blue-400 bg-opacity-50 text-white absolute flex flex-col items-start justify-start gap-10 text-2xl shadow-2xl shadow-black'>
+        <div className='h-screen font-bold font-Roboto w-56 pl-9 pt-24  bg-blue-400 bg-opacity-50 text-white absolute flex flex-col items-start justify-start gap-10 text-2xl shadow-2xl shadow-black'>
             <NavLink to="/">Multiple Point</NavLink>
             <NavLink to="/polygonmap">Polygon</NavLink>
         </div>
